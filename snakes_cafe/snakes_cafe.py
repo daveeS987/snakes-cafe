99b87e846306ce13de934af540a6ae82a6bb081d
Davee Sok <daveesok@gmail.com>
2021-06-05 17:33:45 -0500

    completed mvp

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -38,7 +38,6 @@ Unicorn Tears
 ***********************************
 ''')   
 
-
   menu_items={
     "Wings":0,
     "Cookies": 0,
@@ -55,16 +54,17 @@ Unicorn Tears
     "Unicorn Tears": 0
   }
 
-
+  
   def output_response(response: str) -> str:
-
     if response in menu_items:
       menu_items[response]+=1
       return f'**{menu_items[response]} order of {response} have been added to your meal**'
+    elif response == 'quit':
+      return "Thank you come again"
     else:
       return f'Please select an item from the list. Check your spelling'
 
-  user_answer=input('>')
-
-  if (user_answer): 
-    print (output_response(user_answer))
+  user_answer=None
+  while(user_answer != 'quit'):
+    user_answer=input('>')
+    print(output_response(user_answer))
