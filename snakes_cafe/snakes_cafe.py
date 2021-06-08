eaf8dc970ab4240e243cc7eb81fa2435bafdc7a5
Davee Sok <daveesok@gmail.com>
2021-06-08 12:01:23 -0500

    moved execution code in main script into a function

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,5 +1,21 @@
 from print_menu import print_menu
 
+menu_items = {
+    "Wings": 0,
+    "Cookies": 0,
+    "Spring Rolls": 0,
+    "Salmon": 0,
+    "Steak": 0,
+    "Meat Tornado": 0,
+    "A Literal Garden": 0,
+    "Ice Cream": 0,
+    "Cake": 0,
+    "Pie": 0,
+    "Coffee": 0,
+    "Tea": 0,
+    "Unicorn Tears": 0,
+}
+
 
 def get_summary(obj):
     result = ""
@@ -9,43 +25,30 @@ def get_summary(obj):
     return result
 
 
-if __name__ == "__main__":
-
-    menu_items = {
-        "Wings": 0,
-        "Cookies": 0,
-        "Spring Rolls": 0,
-        "Salmon": 0,
-        "Steak": 0,
-        "Meat Tornado": 0,
-        "A Literal Garden": 0,
-        "Ice Cream": 0,
-        "Cake": 0,
-        "Pie": 0,
-        "Coffee": 0,
-        "Tea": 0,
-        "Unicorn Tears": 0,
-    }
-
-    def output_response(response: str) -> str:
-        if response in menu_items:
-            menu_items[response] += 1
-            return f"**{menu_items[response]} order of {response} have been added to your meal**"
-        elif response == "summary":
-            summary = get_summary(menu_items)
-            return f"""
+def output_response(response: str) -> str:
+    if response in menu_items:
+        menu_items[response] += 1
+        return f"**{menu_items[response]} order of {response} have been added to your meal**"
+    elif response == "summary":
+        summary = get_summary(menu_items)
+        return f"""
 *-----------------------------  Summary  -----------------------------*\n
 {summary}
       """
 
-        elif response == "quit":
-            return "Thank you come again"
-        else:
-            return f"Please select an item from the list. Check your spelling"
+    elif response == "quit":
+        return "Thank you come again"
+    else:
+        return f"Please select an item from the list. Check your spelling"
 
+
+def main():
     print_menu()
-
     user_answer = None
     while user_answer != "quit":
         user_answer = input(">")
         print(output_response(user_answer))
+
+
+if __name__ == "__main__":
+    main()
