a57a25b2a77112a9f5e4049da14d858029a2656d
Davee Sok <daveesok@gmail.com>
2021-06-07 20:12:48 -0500

    moved print statement into its own module. just to practice importing

---
+++ snakes_cafe/print_menu.py
@@ -0,0 +1,44 @@
+def print_menu():
+    print(
+        """
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**                                  **
+** To quit at any time, type "quit" **
+**************************************
+
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+***********************************
+** What would you like to order? **
+***********************************
+"""
+    )
+
+
+if __name__ == "__main__":
+    print("This is the print_menu script")
--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,85 +1,51 @@
-if __name__ == "__main__":
+from print_menu import print_menu
 
-  print('''
-**************************************
-**    Welcome to the Snakes Cafe!   **
-**    Please see our menu below.    **
-**                                  **
-** To quit at any time, type "quit" **
-**************************************
 
-Appetizers
-----------
-Wings
-Cookies
-Spring Rolls
-
-Entrees
--------
-Salmon
-Steak
-Meat Tornado
-A Literal Garden
-
-Desserts
---------
-Ice Cream
-Cake
-Pie
-
-Drinks
-------
-Coffee
-Tea
-Unicorn Tears
-
-***********************************
-** What would you like to order? **
-***********************************
-''')   
-
-  menu_items={
-    "Wings":0,
-    "Cookies": 0,
-    "Spring Rolls": 0,
-    "Salmon": 0,
-    "Steak": 0,
-    "Meat Tornado": 0,
-    "A Literal Garden": 0,
-    "Ice Cream": 0,
-    "Cake": 0,
-    "Pie": 0, 
-    "Coffee": 0,
-    "Tea": 0,
-    "Unicorn Tears": 0
-  }
-
-  def get_summary(obj):
-    result=''
+def get_summary(obj):
+    result = ""
     for item in obj:
-      if(obj[item] > 0):
-        result += f'{obj[item]} {item} \n'
+        if obj[item] > 0:
+            result += f"{obj[item]} {item} \n"
     return result
 
 
-  def output_response(response: str) -> str:
+def output_response(response: str) -> str:
     if response in menu_items:
-      menu_items[response]+=1
-      summary= get_summary(menu_items)
-      return f'''
+        menu_items[response] += 1
+        summary = get_summary(menu_items)
+        return f"""
 **{menu_items[response]} order of {response} have been added to your meal**
 
 *-----------------------------  Summary  -----------------------------*
 {summary}
-      '''
-    elif response == 'quit':
-      return "Thank you come again"
+      """
+    elif response == "quit":
+        return "Thank you come again"
     else:
-      return f'Please select an item from the list. Check your spelling'
+        return f"Please select an item from the list. Check your spelling"
 
 
+if __name__ == "__main__":
 
-  user_answer=None
-  while(user_answer != 'quit'):
-    user_answer=input('>')
-    print(output_response(user_answer))
+    menu_items = {
+        "Wings": 0,
+        "Cookies": 0,
+        "Spring Rolls": 0,
+        "Salmon": 0,
+        "Steak": 0,
+        "Meat Tornado": 0,
+        "A Literal Garden": 0,
+        "Ice Cream": 0,
+        "Cake": 0,
+        "Pie": 0,
+        "Coffee": 0,
+        "Tea": 0,
+        "Unicorn Tears": 0,
+    }
+
+    print_menu()
+
+    user_answer = None
+    while user_answer != "quit":
+        user_answer = input(">")
+        print(output_response(user_answer))
